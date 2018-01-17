c8020fe96832da7910772cd0ba84cc11a0dc9ad3
C?dric Krier <ced@b2ck.com>
2018-01-17 17:30:48 +0100

    Remove volumes from drone and fix test

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,7 +1,9 @@
 # This file is part company_bank module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
-
-from .test_company_bank import suite
+try:
+    from trytond.modules.company_bank.tests.test_company_bank import suite
+except ImportError:
+    from .test_company_bank import suite
 
 __all__ = ['suite']
