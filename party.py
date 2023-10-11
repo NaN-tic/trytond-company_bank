66c9850a344123e00d87daafe9bb1cd4c9124755
Albert Cervera i Areny <albert@nan-tic.com>
2023-10-11 10:35:16 +0200

    Pyflakes fixes

--- party.py
+++ party.py
@@ -1,7 +1,7 @@
 # This file is part of company_bank module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
-from trytond.model import fields, ModelSQL, Unique
+from trytond.model import fields, ModelSQL
 from trytond.pool import Pool, PoolMeta
 from trytond.pyson import Eval
 from trytond.transaction import Transaction
